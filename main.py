af0c77e4b255e576fb81a47ddea15fcbf67947e4
P. Kohinoor <73068072+kohi-noor@users.noreply.github.com>
2021-03-13 09:30:58 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'nora' in command:
+                command = command.replace('nora', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_nora():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is ', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('i am in relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'tum kaise ho' in command:
+        talk('mein thik hoon')
+    elif 'who are you' in command:
+        talk('i am your virtual assistant ')
+    elif 'who created you' in command:
+        talk('i was created by the great great great kohinoor')
+    elif 'where are you from' in command:
+        talk('somewhere in the universe')
+    elif 'chup' in command:
+        talk('i am sorry')
+    elif 'sing a song' in command:
+        talk('o     maan     bhar       gaya     meraaa      saaar  a   rishte   tod       denge jiss    din aadat banegi ussi din he chood denge ')
+
+    else:
+        talk('Please say the command again. ')
+
+while True:
+    run_nora()
